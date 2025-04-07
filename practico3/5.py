f44138c8a7795df05c92280380e1a1505e6d428d
Tomás Maraschio <tomas.maraschio@gmail.com>
2025-04-07 20:02:35 -0300

    incisos e y d

--- practico3/5.py
+++ practico3/5.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from numpy.random import uniform
 from collections.abc import Callable
+from math import e
 
 def monte_carlo(n: int, f: Callable[..., float], n_args: int = 1) -> float:
     summation = 0
@@ -26,3 +27,19 @@ print("\tn = 5000:", monte_carlo(5000, f))
 print("\tn = 10000:", monte_carlo(10000, f))
 print("\tn = 1000000:", monte_carlo(1000000, f))
 print("\tValor real: 0.4904146265")
+
+f = lambda x : (1/x - 1) / ((1 + (1/x - 1)**2)*x)**2
+print("Estimación de integral (0, infinito) de x * (x²+1)⁻²:")
+print("\tn = 1000:", monte_carlo(1000, f))
+print("\tn = 5000:", monte_carlo(5000, f))
+print("\tn = 10000:", monte_carlo(10000, f))
+print("\tn = 1000000:", monte_carlo(1000000, f))
+print("\tValor real: 0.5")
+
+f = lambda x : 2 * e**(-(1/x - 1)**2) / x**2
+print("Estimación de integral (-infinito, infinito) de e^(-x²):")
+print("\tn = 1000:", monte_carlo(1000, f))
+print("\tn = 5000:", monte_carlo(5000, f))
+print("\tn = 10000:", monte_carlo(10000, f))
+print("\tn = 1000000:", monte_carlo(1000000, f))
+print("\tValor real: 1.772453851")
