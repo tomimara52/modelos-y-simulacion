d6aec0e2dbfbd5e4da19f1da3e8e7aa59be49540
Tomás Maraschio <tomas.maraschio@gmail.com>
2025-04-07 18:07:30 -0300

    ej 5, incisos a y b

---
+++ practico3/5.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from numpy.random import uniform
+from collections.abc import Callable
+
+def monte_carlo(f: Callable[[float], float], n: int) -> float:
+    summation = 0
+
+    for _ in range(n):
+        summation += f(uniform())
+
+    return summation / n
+
+
+f = lambda x : (1-x*x)**1.5
+print("Estimación de integral (0, 1) de (1-x²)^(3/2):")
+print("\tn = 1000:", monte_carlo(f, 1000))
+print("\tn = 5000:", monte_carlo(f, 5000))
+print("\tn = 10000:", monte_carlo(f, 10000))
+
+f = lambda x : (x + 2) / ((x + 2)**2 - 1)
+print("Estimación de integral (2, 3) de x / (x²-1):")
+print("\tn = 1000:", monte_carlo(f, 1000))
+print("\tn = 5000:", monte_carlo(f, 5000))
+print("\tn = 10000:", monte_carlo(f, 10000))
