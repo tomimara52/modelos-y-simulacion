e491b99de2f4db8b29dc63ccf72f960c2dcbe981
Tomás Maraschio <tomas.maraschio@gmail.com>
2025-04-07 20:28:38 -0300

    terminar ej 5

--- practico3/5.py
+++ practico3/5.py
@@ -43,3 +43,21 @@ print("\tn = 5000:", monte_carlo(5000, f))
 print("\tn = 10000:", monte_carlo(10000, f))
 print("\tn = 1000000:", monte_carlo(1000000, f))
 print("\tValor real: 1.772453851")
+
+
+f = lambda x, y : e**((x + y)**2)
+print("Estimación de integral (0, 1) (0, 1) de e^(-(x+y)²):")
+print("\tn = 1000:", monte_carlo(1000, f, 2))
+print("\tn = 5000:", monte_carlo(5000, f, 2))
+print("\tn = 10000:", monte_carlo(10000, f, 2))
+print("\tn = 1000000:", monte_carlo(1000000, f, 2))
+print("\tValor real: 4.89916")
+
+
+f = lambda x, y : e**(-(1/x + 1/y - 2)) / (x*y)**2 if 1/x - 1 > 1/y - 1 else 0
+print("Estimación de integral (0, infinito) (0, x) de e^-(x+y):")
+print("\tn = 1000:", monte_carlo(1000, f, 2))
+print("\tn = 5000:", monte_carlo(5000, f, 2))
+print("\tn = 10000:", monte_carlo(10000, f, 2))
+print("\tn = 1000000:", monte_carlo(1000000, f, 2))
+print("\tValor real: 0.5")
