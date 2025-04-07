5eff7d1719d164b1c613d85bafaf5ba7e02f0c29
Tomás Maraschio <tomas.maraschio@gmail.com>
2025-04-07 20:01:48 -0300

    cambiar orden de los parametros y agregar la cantidad de parametros que toma f

--- practico3/5.py
+++ practico3/5.py
@@ -2,7 +2,7 @@
 from numpy.random import uniform
 from collections.abc import Callable
 
-def monte_carlo(f: Callable[[float], float], n: int) -> float:
+def monte_carlo(n: int, f: Callable[..., float], n_args: int = 1) -> float:
     summation = 0
 
     for _ in range(n):
@@ -13,14 +13,16 @@ def monte_carlo(f: Callable[[float], float], n: int) -> float:
 
 f = lambda x : (1-x*x)**1.5
 print("Estimación de integral (0, 1) de (1-x²)^(3/2):")
-print("\tn = 1000:", monte_carlo(f, 1000))
-print("\tn = 5000:", monte_carlo(f, 5000))
-print("\tn = 10000:", monte_carlo(f, 10000))
+print("\tn = 1000:", monte_carlo(1000, f))
+print("\tn = 5000:", monte_carlo(5000, f))
+print("\tn = 10000:", monte_carlo(10000, f))
+print("\tn = 1000000:", monte_carlo(1000000, f))
 print("\tValor real: 0.589048622")
 
 f = lambda x : (x + 2) / ((x + 2)**2 - 1)
 print("Estimación de integral (2, 3) de x / (x²-1):")
-print("\tn = 1000:", monte_carlo(f, 1000))
-print("\tn = 5000:", monte_carlo(f, 5000))
-print("\tn = 10000:", monte_carlo(f, 10000))
+print("\tn = 1000:", monte_carlo(1000, f))
+print("\tn = 5000:", monte_carlo(5000, f))
+print("\tn = 10000:", monte_carlo(10000, f))
+print("\tn = 1000000:", monte_carlo(1000000, f))
 print("\tValor real: 0.4904146265")
