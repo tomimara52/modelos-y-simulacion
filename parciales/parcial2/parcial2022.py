d73bbf1f2100934d5a08f3161c5c06b5d7c068d5
Tomás Maraschio <tomas.maraschio@gmail.com>
2025-05-13 11:11:25 -0300

    parcial 2022 hasta ej 3

---
+++ parciales/parcial2/parcial2022.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+from math import log, exp, inf, sqrt
+from numpy.random import uniform
+from scipy.integrate import quad
+
+
+# EJ 1
+A = [0] * 32 + [1] * 21 + [2] * 33 + [3] * 14
+
+def urna() -> int:
+    random_index = int(uniform() * 100)
+    return A[random_index]
+
+freqs = [0] * 4
+for _ in range(10_000):
+    freqs[urna()] += 1
+
+print('---------- Ejercicio 1 ----------')
+for (i, f) in enumerate(freqs):
+    print(f'Estimación de P(X = {i}): {f / 10_000}')
+
+
+
+# EJ 2
+
+def transformada_inversa() -> float:
+    U = uniform()
+    if U < 1 / 3:
+        return log(3 * U)
+    else:
+        return 0.5 * log(1 / (1.5 * (1 -U)))
+
+
+print('---------- Ejercicio 2 ----------')
+f = lambda x: (1 / 3) * exp(x) if x <= 0 else (4 / 3) * exp(-2 * x)
+print(f'P(X <= 1) = {quad(f, -inf, 1)[0]}')
+
+less_than_1 = 0
+for _ in range(10_000):
+    if transformada_inversa() <= 1:
+        less_than_1 += 1
+print(f'Estimación de P(X <= 1) = {less_than_1 / 10_000}')
