ac7a3c6abe309d7634fb290b3bf492947c183d71
Tomás Maraschio <tomas.maraschio@gmail.com>
2025-06-03 19:35:03 -0300

    practico 6 ej 9

---
+++ practico6/9.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+from typing import Callable
+from numpy.random import uniform
+from math import log
+import matplotlib.pyplot as plt
+
+
+def poisson_process(T: float, l: float, l_t: Callable[[float], float]) -> list[float]:
+    events = []
+    t = -log(1 - uniform()) / l
+
+    while t <= T:
+        V = uniform()
+        if V < l_t(t) / l:
+            events.append(t)
+        t += -log(1 - uniform()) / l
+
+    return events
+
+
+def lambda_t(t: float) -> float:
+    x = t % 10
+
+    if x <= 5:
+        return 3 * x + 4
+    else:
+        return 19 - 3 * (x - 5)
+
+
+def get_jobs_before(t: float, jobs: list[float]) -> list[float]:
+    i = 0
+
+    while i < len(jobs) and jobs[i] <= t:
+        i += 1
+
+    return jobs[:i]
+
+
+def simulate_server(T: float, debug: bool = False) -> tuple[float, float]:
+    job_events = poisson_process(T, 19, lambda_t)
+
+    if debug:
+        print(job_events)
+
+    time = 0
+    total_sleep_time = 0
+
+    while job_events != []:
+        programmed_jobs = get_jobs_before(time, job_events)
+
+        if programmed_jobs == []:
+            # sleep
+            sleep_time = uniform() * 0.3
+
+            if debug:
+                print(f"Sleep from {time} to {time + sleep_time}")
+
+            time += sleep_time
+            total_sleep_time += sleep_time
+        else:
+            for _ in range(len(programmed_jobs)):
+                # service time
+                work_time = -log(uniform()) / 25
+
+                if debug:
+                    print(f"Work from {time} to {time + work_time}")
+
+                time += work_time
+
+            job_events = job_events[len(programmed_jobs) :]
+
+    return time, total_sleep_time
+
+
+def estimate_sleep_time(T: float, n_sims: int) -> tuple[float, list[float]]:
+    acc = 0
+    sleep_times = []
+
+    for _ in range(n_sims):
+        _, sleep_time = simulate_server(T)
+        acc += sleep_time
+        sleep_times.append(sleep_time)
+
+    return acc / n_sims, sleep_times
+
+
+estimated_sleep_time, sleep_times = estimate_sleep_time(100, 5000)
+print(
+    f"Tiempo que el servidor está detenido estimado en las primeras 100 horas: {estimated_sleep_time}"
+)
+
+
+# lógica para hacer que los bins vayan en incrementos de medio segundo
+min_sleep_time = min(sleep_times)
+max_sleep_time = max(sleep_times)
+lower_edge = (
+    int(min_sleep_time)
+    if min_sleep_time - int(min_sleep_time) < 0.5
+    else int(min_sleep_time) + 0.5
+)
+upper_edge = (
+    int(max_sleep_time) + 0.5
+    if max_sleep_time - int(max_sleep_time) < 0.5
+    else int(max_sleep_time) + 1
+)
+
+bin_edges = []
+i = lower_edge
+while i <= upper_edge:
+    bin_edges.append(i)
+    i += 0.5
+
+
+_, bins, _ = plt.hist(sleep_times, bins=bin_edges, color="skyblue", edgecolor="black")
+plt.xticks(bins, [f"{edge:.1f}" for edge in bins], rotation=50, fontsize=8)
+plt.title("Histograma de tiempos detenido en las primeras 100 horas")
+plt.xlabel("Tiempo en segundos")
+plt.ylabel("Frecuencia")
+
+plt.savefig("histograma.png", dpi=300, bbox_inches="tight")
+plt.show()
